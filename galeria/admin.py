7ef3365448ad8760225f72826e0cec5d8f17737e
LetíciaCastelo <leticiacastelo.dev@gmail.com>
2024-05-31 23:57:14 -0300

    update admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from galeria.models import Fotografia
 
 class ListandoFotografias (admin.ModelAdmin):
-  list_display = ("id", "nome", "legenda")
+  list_display = ("id", "nome", "legenda", "publicada",)
   list_display_links = ("id", "nome")
   search_fields = ("nome",) # campo de busca 
+  list_filter = ("categoria",) # filtro por categoria
+  list_per_page = 10 # itens por página
+  
 
 admin.site.register(Fotografia, ListandoFotografias)
