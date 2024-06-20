6095abcf3636c79e03bda9560dbc1c89b7ee53f0
LetíciaCastelo <leticiacastelo.dev@gmail.com>
2024-06-20 18:21:54 -0300

    forms

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -8,7 +8,8 @@ class LoginForms(forms.Form):
     max_length = 100,
     widget= forms.TextInput(
       attrs = {
-        "class": "form-group"
+        "class": "form-control",
+        "placeholder": "Ana Caroline"
       }
     )
   )
@@ -19,9 +20,55 @@ class LoginForms(forms.Form):
     max_length = 70,
     widget = forms.PasswordInput(
       attrs = {
-      "class": "form-group"
+      "class": "form-control",
+      "placeholder": "Digite sua senha"
       }
     )
   )
   
+class CadastroForms(forms.Form):
+  nome_cadastro = forms.CharField(
+    label = "Nome de Cadastro",
+    required= True,
+    max_length= 100,
+    widget= forms.TextInput(
+      attrs= {
+        "class": "form-control",
+        "placeholder":"Ana Caroline Branco"
+      }
+    )
+  )
+  email = forms.EmailField(
+  label = "Email",
+  required = True,
+  max_length= 100,
+  widget = forms.EmailInput(
+    attrs= {
+        "class": "form-control",
+        "placeholder":"Ex.: anabranco@gmail.com"
+      }
+    )   
+  )
+  senha_1 = forms.CharField(
+    label = "Senha",
+    required = True,
+    max_length = 70,
+    widget = forms.PasswordInput(
+      attrs = {
+      "class": "form-control",
+      "placeholder": "Digite sua senha"
+      }
+    )
+  )
+  senha_2 = forms.CharField(
+    label = "Confirme sua senha",
+    required = True,
+    max_length = 70,
+    widget = forms.PasswordInput(
+      attrs = {
+      "class": "form-control",
+      "placeholder": "Digite sua senha novamente"
+      }
+    )
+  )
   
