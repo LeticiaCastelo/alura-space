b9598a41cbf0df5992c69b173373671465983a27
LetíciaCastelo <leticiacastelo.dev@gmail.com>
2024-06-19 23:55:41 -0300

    forms

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,6 +1,27 @@
 from django import forms
+#Cria o formulário de Login
 
 class LoginForms(forms.Form):
   nome_login = forms.CharField(
-    
+    label = "Nome de Login",
+    required = True,
+    max_length = 100,
+    widget= forms.TextInput(
+      attrs = {
+        "class": "form-group"
+      }
+    )
   )
+  
+  senha = forms.CharField(
+    label = "Senha",
+    required = True,
+    max_length = 70,
+    widget = forms.PasswordInput(
+      attrs = {
+      "class": "form-group"
+      }
+    )
+  )
+  
+  
